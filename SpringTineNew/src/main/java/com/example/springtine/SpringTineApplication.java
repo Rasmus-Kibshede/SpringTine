7a5320090415083205edef070cda68a9ae2adc82
Rasmus Kibshede <2740ralle@gmail.com>
2021-09-28 09:46:27 +0200

    Spring with Tine

---
+++ SpringTineNew/src/main/java/com/example/springtine/SpringTineApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTineApplication.class, args);
+    }
+
+}
---
+++ SpringTineNew/src/test/java/com/example/springtine/SpringTineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
